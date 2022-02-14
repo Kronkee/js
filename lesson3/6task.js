7a417d272be2537aab5a5e6c74f474577691c34c
Екатерина Богопольская <Katya.sovie@gmail.com>
2022-02-14 23:03:53 +0300

    add html, js for lesson-3

---
+++ lesson3/6task.js
@@ -0,0 +1,10 @@
+"use strict";
+//ЗАДАНИЕ 6
+
+let i;
+let element = ["x"];
+console.log(element.join(""));
+for (i = 0; i < 19; i++) {
+    element.push("x");
+    console.log(element.join(""));
+}
